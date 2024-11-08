92d21cae730415b36ef47c11bf58cf2ced7110e2
Stinginend <stingingend@icloud.com>
2024-11-08 18:47:33 -0500

    1

--- js/arith.js
+++ js/arith.js
@@ -4,13 +4,12 @@ let spanE2 = document.getElementById('num2');
 
 // element --- property
 //var num1 = parseInt(document.getElementById('n1').textContent); 
-let n1 = spanE1.textContent;
-let n2 = spanE2.textContent;
+let n1 = parseInt(spanE1.textContent);
+let n2 = parseInt(spanE2.textContent);
 console.log(typeof n1);
 
 //process
-let num1 = parseInt(document.getElementById('n1').textContent); 
-let num2 = parseInt(document.getElementById('n2').textContent); 
-let c = num1 + num2;
+var c = n1 + n2;
+document.write(c);
 //display
 
